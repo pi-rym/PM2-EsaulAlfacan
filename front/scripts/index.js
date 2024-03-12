80317d8f0ea587a16d94bc9879c220d9b6ace35a
EsaulAlfacan <esaulalfacan@gmail.com>
2024-03-12 12:02:32 -0600

    Asincronismo: Actividad 1,2 terminadas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,27 @@
+// Importación del módulo renderCard desde el archivo renderCard.js
 const renderCard = require("./renderCard");
+// Importación del módulo axios
+const axios = require("axios");
 
-$.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
-  renderCard(data);
-});
+// Declaración de una arrow function expresada
+const fGetAxios = async (data) => {
+  // Control del flujo de la solicitud con axios
+  try {
+    // Petición a el servidor
+    const peticion = await axios.get(
+      "https://students-api.up.railway.app/movies"
+    );
+
+    // Validación para una petición exitosa
+    if (peticion.status === 200) {
+      const movieData = peticion.data;
+      renderCard(movieData);
+    } else {
+      return `La solicitud no fue exitosa debido a el problema ${peticion.status} `;
+    }
+  } catch (error) {
+    `La solicitud no se genero correctamente, se muestra${error}`;
+  }
+};
+
+fGetAxios();
